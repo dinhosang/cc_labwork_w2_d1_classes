854782c9621e6994d092a63fbf39728040d158e9
Douglas Sangster <d.sangster@hotmail.co.uk>
2017-11-28 01:37:24 +0000

    changed initialize for Library class to have the user input the starting book collection, rather than having the class come with them

--- library_class.rb
+++ library_class.rb
@@ -2,31 +2,14 @@ class Library
 
   attr_reader :books
 
-  def initialize
+  def initialize(*books)
 
-    @books = [
-      {
-      title: "lord_of_the_rings",
-      rental_details: {
-       student_name: "Jeff",
-       date: "01/12/17"
-                      }
-      },
-      {
-      title: "count_of_monte_cristo",
-      rental_details: {
-       student_name: "Lea",
-       date: "16/12/17"
-                      }
-      },
-      {
-      title: "puzzler's_dilemma",
-      rental_details: {
-       student_name: "Roarke",
-       date: "13/01/18"
-                      }
-      }
-    ]
+    # reminder to self:
+    # *books in the parameter means all the entered args
+    # are placed into the class in an array
+    # this array is then entered straight into @books
+    @books = books
+
   end
 
 
--- specs/library_class_spec.rb
+++ specs/library_class_spec.rb
@@ -5,7 +5,29 @@ require_relative('../library_class')
 class TestLibrary < MiniTest::Test
 
   def setup
-    @the_library = Library.new()
+    @the_library = Library.new(
+      {
+      title: "lord_of_the_rings",
+      rental_details: {
+       student_name: "Jeff",
+       date: "01/12/17"
+                      }
+      },
+      {
+      title: "count_of_monte_cristo",
+      rental_details: {
+       student_name: "Lea",
+       date: "16/12/17"
+                      }
+      },
+      {
+      title: "puzzler's_dilemma",
+      rental_details: {
+       student_name: "Roarke",
+       date: "13/01/18"
+                      }
+      }
+    )
   end
 
 
